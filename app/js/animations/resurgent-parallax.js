ef8e189e2fd17d577a09e082ed396b3c354cc255
Logan Bernard <bernard.logan4@gmail.com>
2015-10-26 20:06:43 -0400

    [home] fixed transition hero image

--- app/js/animations/resurgent-parallax.js
+++ app/js/animations/resurgent-parallax.js
@@ -10,5 +10,6 @@ $(window).bind('scroll',function(e){
 function parallaxScroll(){
   var scrolled = $(window).scrollTop();
   // $('.transition-overlay').css('top',(0-(scrolled*.5))+'px');
-  $('.transition-hero').css('top',(0-(scrolled*.3))+'px');
+  $('.transition-hero').css('top',(500-(scrolled*.3))+'px');
+  console.log(scrolled);
 }
